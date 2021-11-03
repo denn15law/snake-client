faff1f59ed48838eb21f4b3c1f6f976624a54644
Dennis Law <denn15_law@live.ca>
2021-11-03 18:29:12 +0000

    create input.js and export setUpInput function to play.js

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
-const net = require("net");
 const { connect } = require("./client");
-console.log("Connecting...");
+const { setUpInput } = require("./input");
 
+console.log("Connecting...");
 connect();
 
-const setUpInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function (key) {
-    if (key === "\u0003") {
-      process.exit();
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
 setUpInput();
-
-// module.exports = setUpInput;
